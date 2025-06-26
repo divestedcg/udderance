efb9dc25ede1c3ae09f6ccac3cca1e7c70ad37a9
Tavi <tavi@divested.dev>
2025-06-26 17:13:39 -0400

    Add script to package assets for Android
    Signed-off-by: Tavi <tavi@divested.dev>

---
+++ package-android.sh
@@ -0,0 +1,22 @@
+#package the Android assets
+rm -rf output-android android-latest.zip;
+cp -r --reflink=auto output output-android;
+#dedupe
+rm output-android/pages/sherpa-onnx-wasm-main-tts.data;
+mv output-android/assets/sherpa-onnx/sherpa-onnx-wasm-main-tts.data output-android/pages/sherpa-onnx-wasm-main-tts.data;
+#remove unnecessary
+rm output-android/index.html;
+rm output-android/assets/aac.json;
+#TODO clean fonts
+rm output-android/assets/css/splash.css;
+rm output-android/assets/js/aac_pictogram_map-todo.js;
+rm output-android/pages/home.html;
+rm output-android/pages/privacy_policy.html;
+rm output-android/pages/search.html;
+rm output-android/pages/terms_of_service.html;
+rm -rfv output-android/images/;
+rm -rfv output-android/pages/pagefind/;
+cd output-android;
+zip -r ../android-latest.zip .;
+cd ..;
+rm -rf output-android;
